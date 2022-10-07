978232bcb2621af03a28b766063615d70dfb8ca4
Sean Kelliher <sbkelliher@icloud.com>
2022-10-07 15:25:29 -0400

    Modify comments style. Modify width of 'bar' class.

--- app/elements.js
+++ app/elements.js
@@ -1,39 +1,42 @@
-//Import the "group" objects from groups.js.
-//Loop through them to create elements on page.
+// Import the "group" objects from groups.js.
+// Loop through them to create elements on page.
 
 import {years, winters, springs, summers, falls} from "./groups.js";
 
 function buildSection(yearOrSeason, key, sectionId) {
 
-    //Retrieve the correct section.
+    // Retrieve the correct section.
     const section = document.getElementById(sectionId);
 
-    //Create elements needed.
+    // Create elements needed.
     const figure = document.createElement("figure");
     const year = document.createElement("span");
     const bar = document.createElement("span");
     const temp = document.createElement("span");
 
-    //Assign class names.
+    // Assign class names.
     year.className = "year";
     bar.className = "bar";
     temp.className = "temp";
 
-    //Degree symbol (needed below).
+    // Degree symbol (needed below).
     const degree = String.fromCharCode(176);
 
-    //Assign text.
+    // Assign text.
     year.textContent = key.substring(1, 5);
     temp.textContent = `${yearOrSeason[key]}` + degree + `F`;
 
-    //Set the bar width.
-    //70% space available.
-    //70 / 51.4 highest avg temp.
-    //Each degree gets 1.36% of space.
+    // Set the bar width.
+    // approximately 75% space available for bars.
+    // 75 / 51.4 highest avg temp.
+    // Each degree gets 1.46% of space.
     const barWidth = Number(`${yearOrSeason[key]}`);
-    bar.style.width = (barWidth * 1.36) + `%`;
+    bar.style.width = (barWidth * 1.46) + `%`;
 
-    //Append elements.
+    // Add class for animation, bars fade in.
+    bar.classList.add("bar-action");
+
+    // Append elements.
     figure.appendChild(year);
     figure.appendChild(bar);
     figure.appendChild(temp);
