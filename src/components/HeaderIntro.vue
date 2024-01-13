26626da6116b348f426b75957beb39bfd62a24f7
Sean Kelliher <sbkelliher@icloud.com>
2024-01-13 12:18:18 -0500

    Remove message about tapping to show/hide month.

--- src/components/HeaderIntro.vue
+++ src/components/HeaderIntro.vue
@@ -4,7 +4,7 @@
 <template>
     <header>
         <h1>Mt. Washington Temperatures</h1>
-        <p>Average monthly temperatures at the Mt. Washington Observatory, located on the summit of Mt. Washington, 6,288 ft (1,916.6 m) above sea level in North Conway, New Hampshire. <em>Tap a month to hide/show its data.</em></p>
+        <p>Average monthly temperatures (&deg;F) at the Mt. Washington Observatory, located on the summit of Mt. Washington, 6,288 ft (1,916.6 m) above sea level in North Conway, New Hampshire.</p>
     </header>
 </template>
 
