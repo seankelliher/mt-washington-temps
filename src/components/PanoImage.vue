8eee54cae648407d124e90a93e45d741f1104953
Sean Kelliher <sbkelliher@icloud.com>
2024-03-13 00:04:15 -0400

    Modify img src to local version of summit photo.

--- src/components/PanoImage.vue
+++ src/components/PanoImage.vue
@@ -7,7 +7,10 @@
             <h1>How cold is the Northeast's highest point?</h1>
         </div>
         <figure>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/View_from_the_top_of_Mt._Washington_%2849675348518%29.jpg">
+            <img
+                src="/mount-washington-summit-andrew-weitzel.jpg"
+                alt="summit of Mount Washington covered in snow and ice"
+            >
             <figcaption>The summit of Mount Washington, March 16, 2020. Photo: <a href="https://commons.wikimedia.org/wiki/File:View_from_the_top_of_Mt._Washington_(49675348518).jpg" target="_blank">Andrew Weitzel</a>.</figcaption>
         </figure>
     </div>
