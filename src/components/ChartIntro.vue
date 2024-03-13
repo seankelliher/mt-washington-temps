706f843c27725fcce38eb461437f1af0b86efccf
Sean Kelliher <sbkelliher@icloud.com>
2024-03-12 23:49:08 -0400

    Fix typo.

--- src/components/ChartIntro.vue
+++ src/components/ChartIntro.vue
@@ -3,7 +3,7 @@
 
 <template>
     <section>
-        <p>Mount Washington, located in North Conway, New Hampshire, is the tallest point in the Northeastern United States at 6,288 feet (1,916.6 meters) above sea level. It's Mount Washington Observatory operates year-round monitoring the mountain's weather. The below chart displays average monthly temperatures on the summit from 2005 - 2023.</p>
+        <p>Mount Washington, located in North Conway, New Hampshire, is the tallest point in the Northeastern United States at 6,288 feet (1,916.6 meters) above sea level. Its Mount Washington Observatory operates year-round monitoring the mountain's weather. The below chart displays average monthly temperatures on the summit from 2005 - 2023.</p>
     </section>
 </template>
 
