c51a1707965c99ce079ed748d5b0691659524ae0
Sean Kelliher <sbkelliher@icloud.com>
2024-05-07 15:04:02 -0400

    Modify text in figcaption.

--- src/components/PanoImage.vue
+++ src/components/PanoImage.vue
@@ -11,7 +11,7 @@
                 src="/mount-washington-summit-andrew-weitzel.jpg"
                 alt="summit of Mount Washington covered in snow and ice"
             >
-            <figcaption>The summit of Mount Washington, March 16, 2020. Photo: <a href="https://commons.wikimedia.org/wiki/File:View_from_the_top_of_Mt._Washington_(49675348518).jpg" target="_blank">Andrew Weitzel</a>.</figcaption>
+            <figcaption>Mount Washington, located in North Conway, New Hampshire, is the tallest point in the Northeastern United States at 6,288 feet (1,916.6 meters) above sea level. Its Mount Washington Observatory, located on the summit, operates year-round monitoring the mountain's weather. Depicted above: Outside the Observatory, March 16, 2020. Photo: <a href="https://commons.wikimedia.org/wiki/File:View_from_the_top_of_Mt._Washington_(49675348518).jpg" target="_blank">Andrew Weitzel</a>.</figcaption>
         </figure>
     </div>
 </template>
