c341e70058273ec60c40bfde9de7a404c00022b3
Sean Kelliher <sbkelliher@icloud.com>
2024-05-07 15:02:42 -0400

    Remove ChartIntro.vue. No longer needed.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
 import PanoImage from "./components/PanoImage.vue";
-import ChartIntro from "./components/ChartIntro.vue";
 import TempsChart from "./components/TempsChart.vue";
 </script>
 
 <template>
     <div id="container">
         <PanoImage />
-        <ChartIntro />
         <TempsChart />
     </div>
 </template>
