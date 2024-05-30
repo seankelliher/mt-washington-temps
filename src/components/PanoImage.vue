36e3007c7f32f190f872f40c1a0bd95da088c559
Sean Kelliher <sbkelliher@icloud.com>
2024-05-29 23:21:44 -0400

    Modify classes for headline and text. Modify text too.

--- src/components/PanoImage.vue
+++ src/components/PanoImage.vue
@@ -3,15 +3,17 @@
 
 <template>
     <div class="pano">
-        <div class="pano-text">
+        <div class="pano-headline">
             <h1>How cold is the Northeast's highest point?</h1>
         </div>
+
+        <div class="pano-text">
+            <p>The Mt. Washington Observatory on the mountain's summit is the highest point in the Northeast U.S. at 6,288 feet (1,916.6 meters) above sea level in North Conway, New Hampshire. Photo: <a href="https://commons.wikimedia.org/wiki/File:View_from_the_top_of_Mt._Washington_(49675348518).jpg" target="_blank">Andrew Weitzel</a>, March 16, 2020.</p>
+        </div>
+
         <figure>
-            <img
-                src="/mount-washington-summit-andrew-weitzel.jpg"
-                alt="summit of Mount Washington covered in snow and ice"
-            >
-            <figcaption>Mount Washington, located in North Conway, New Hampshire, is the tallest point in the Northeastern United States at 6,288 feet (1,916.6 meters) above sea level. Its Mount Washington Observatory, located on the summit, operates year-round monitoring the mountain's weather. Depicted above: Outside the Observatory, March 16, 2020. Photo: <a href="https://commons.wikimedia.org/wiki/File:View_from_the_top_of_Mt._Washington_(49675348518).jpg" target="_blank">Andrew Weitzel</a>.</figcaption>
+            <!-- image added as background img -->
+            <figcaption hidden>Summit of Mount Washington covered in snow and ice.</figcaption>
         </figure>
     </div>
 </template>
