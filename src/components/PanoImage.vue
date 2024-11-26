08cca2ce87372d7b8aeee526c422bada2e17a1ba
Sean Kelliher <sbkelliher@icloud.com>
2024-11-25 22:58:47 -0500

    Shorten headline.

--- src/components/PanoImage.vue
+++ src/components/PanoImage.vue
@@ -4,14 +4,14 @@
 <template>
     <div class="pano">
         <div class="pano-headline">
-            <h1 class="bevan-regular">How cold is the Northeast's highest point?</h1>
+            <h1 class="bevan-regular">How cold is Mt. Washington?</h1>
         </div>
 
-        <figure class="pano-figure">
+        <figure class="full-width">
             <img
                 src="/mount-washington-summit-dept-defense.jpg"
                 alt="climber unfurls a Marines flag on the frigid, snowy summit of Mt. Washington"
-                class="pano-img"
+                class="full-width-img"
             >
             <figcaption hidden>US Marine Petty Officer 3rd Class Jonathan Szylobry. Photo: Department of Defense</figcaption>
         </figure>
