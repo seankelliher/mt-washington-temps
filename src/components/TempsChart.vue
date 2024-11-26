ead8523e3ac1357c8606837e75a0ce356618c93c
Sean Kelliher <sbkelliher@icloud.com>
2024-11-25 19:31:08 -0500

    Remove pano caption. Put in its won component.

--- src/components/TempsChart.vue
+++ src/components/TempsChart.vue
@@ -211,8 +211,6 @@ onMounted(()=> {
 </script>
 
 <template>
-    <div class="pano-caption roboto-serif four">Petty Officer 3rd Class Jonathan Szylobry, a US Marine, on the summit of Mt. Washington in North Conway, New Hampshire. At 6,288 feet (1,916.6 meters) above sea level, it is the highest point in the Northeast United States. Photo: <a href="https://commons.wikimedia.org/wiki/File:USMC-080103-M-0000M-01.jpg" target="_blank">Department of Defense</a>.</div>
-
     <div class="chart">
         <div class="chart-title">
             <h2 class="bevan-regular">Mt. Washington</h2>
