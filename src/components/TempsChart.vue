057e273bc13e11e6452e1577eeae723da5e6b842
Sean Kelliher <sbkelliher@icloud.com>
2024-11-26 15:29:43 -0500

    Remove most functionality out of TempsChart.vue and into store.js.

--- src/components/TempsChart.vue
+++ src/components/TempsChart.vue
@@ -1,62 +1,8 @@
 <script setup>
-import { monthlyTemps } from "../data/temps.js";
-import { yearsList } from "../composables/store.js";
+import { store } from "../composables/store.js";
+import { yearsList } from "../data/years.js";
 import Chart from "chart.js/auto";
-import { ref, onMounted } from "vue";
-
-const years = [
-    "y2005",
-    "y2006",
-    "y2007",
-    "y2008",
-    "y2009",
-    "y2010",
-    "y2011",
-    "y2012",
-    "y2013",
-    "y2014",
-    "y2015",
-    "y2016",
-    "y2017",
-    "y2018",
-    "y2019",
-    "y2020",
-    "y2021",
-    "y2022",
-    "y2023"
-];
-
-const jans = ref([]);
-const febs = ref([]);
-const mars = ref([]);
-const aprs = ref([]);
-const mays = ref([]);
-const juns = ref([]);
-const juls = ref([]);
-const augs = ref([]);
-const seps = ref([]);
-const octs = ref([]);
-const novs = ref([]);
-const decs = ref([]);
-
-function getMonthly() {
-    years.forEach((year) => {
-        jans.value.push(monthlyTemps[year].jan.avgMonthly);
-        febs.value.push(monthlyTemps[year].feb.avgMonthly);
-        mars.value.push(monthlyTemps[year].mar.avgMonthly);
-        aprs.value.push(monthlyTemps[year].apr.avgMonthly);
-        mays.value.push(monthlyTemps[year].may.avgMonthly);
-        juns.value.push(monthlyTemps[year].jun.avgMonthly);
-        juls.value.push(monthlyTemps[year].jul.avgMonthly);
-        augs.value.push(monthlyTemps[year].aug.avgMonthly);
-        seps.value.push(monthlyTemps[year].sep.avgMonthly);
-        octs.value.push(monthlyTemps[year].oct.avgMonthly);
-        novs.value.push(monthlyTemps[year].nov.avgMonthly);
-        decs.value.push(monthlyTemps[year].dec.avgMonthly);
-    });
-}
-
-getMonthly();
+import { onMounted } from "vue";
 
 // Function to create a chart with Chart.js.
 function createMonthsChart() {
@@ -69,7 +15,7 @@ function createMonthsChart() {
             datasets: [
                 {
                     label: "Jan",
-                    data: jans.value,
+                    data: store.jans,
                     backgroundColor: "#1e88e5", // blue 600.
                     borderColor: "#1e88e5",
                     borderWidth: 1,
@@ -78,7 +24,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Feb",
-                    data: febs.value,
+                    data: store.febs,
                     backgroundColor: "#0d47a1", // blue 900.
                     borderColor: "#0d47a1",
                     borderWidth: 1,
@@ -87,7 +33,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Mar",
-                    data: mars.value,
+                    data: store.mars,
                     backgroundColor: "#81c784", // green 300.
                     borderColor: "#81c784",
                     borderWidth: 1,
@@ -96,7 +42,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Apr",
-                    data: aprs.value,
+                    data: store.aprs,
                     backgroundColor: "#43a047", // green 600.
                     borderColor: "#43a047",
                     borderWidth: 1,
@@ -106,7 +52,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "May",
-                    data: mays.value,
+                    data: store.mays,
                     backgroundColor: "#1b5e20", // green 900.
                     borderColor: "#1b5e20",
                     borderWidth: 1,
@@ -115,7 +61,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Jun",
-                    data: juns.value,
+                    data: store.juns,
                     backgroundColor: "#ff8a65", // deep orange 300.
                     borderColor: "#ff8a65",
                     borderWidth: 1,
@@ -124,7 +70,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Jul",
-                    data: juls.value,
+                    data: store.juls,
                     backgroundColor: "#f4511e", // deep orange 600.
                     borderColor: "#f4511e",
                     borderWidth: 1,
@@ -133,7 +79,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Aug",
-                    data: augs.value,
+                    data: store.augs,
                     backgroundColor: "#bf360c", // deep orange 900.
                     borderColor: "#bf360c",
                     borderWidth: 1,
@@ -142,7 +88,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Sep",
-                    data: seps.value,
+                    data: store.seps,
                     backgroundColor: "#a1887f", // brown 300.
                     borderColor: "#a1887f",
                     borderWidth: 1,
@@ -151,7 +97,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Oct",
-                    data: octs.value,
+                    data: store.octs,
                     backgroundColor: "#6d4c41", // brown 600.
                     borderColor: "#6d4c41",
                     borderWidth: 1,
@@ -160,7 +106,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Nov",
-                    data: novs.value,
+                    data: store.novs,
                     backgroundColor: "#3e2723", // brown 900.
                     borderColor: "#3e2723",
                     borderWidth: 1,
@@ -169,7 +115,7 @@ function createMonthsChart() {
                 },
                 {
                     label: "Dec",
-                    data: decs.value,
+                    data: store.decs,
                     backgroundColor: "#64b5f6", // blue 300.
                     borderColor: "#64b5f6",
                     borderWidth: 1,
@@ -206,6 +152,7 @@ function createMonthsChart() {
 }
 
 onMounted(()=> {
+    store.getMonthly();
     createMonthsChart();
 });
 </script>
@@ -213,8 +160,8 @@ onMounted(()=> {
 <template>
     <div class="chart">
         <div class="chart-title">
-            <h2 class="bevan-regular">Mt. Washington</h2>
-            <h3 class="roboto-serif five">Average monthly summit temperatures</h3>
+            <h2 class="bevan-regular">Monthly temperatures</h2>
+            <p class="roboto-serif four">Average monthly temperatures for each month, 2005 - 2023</p>
         </div>
         <canvas id="monthly-temps-chart"></canvas>
         <div class="chart-caption roboto-serif four">Data from the Mt. Washington Observatory, a weather station located on the mountain's summit. The Observatory monitors weather year round.</div>
