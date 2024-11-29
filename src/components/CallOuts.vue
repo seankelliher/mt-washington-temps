cb6aae4be4b8df289f50000159b60132edc3fe5e
Sean Kelliher <sbkelliher@icloud.com>
2024-11-29 12:14:01 -0500

    Add hgroup tag to callouts.

--- src/components/CallOuts.vue
+++ src/components/CallOuts.vue
@@ -4,28 +4,26 @@ import { store } from "../composables/store.js";
 
 <template>
     <section>
-        <div class="heading">
+        <hgroup class="heading">
             <h2 class="bevan-regular">Seasonal temperatures</h2>
-        </div>
-        <div class="heading">
             <p class="roboto-serif four">Average temperatures, based on the years 2005 - 2023</p>
-        </div>
-        <div class="callout">
+        </hgroup>
+        <hgroup class="callout">
             <h3 class="roboto-serif six">Spring</h3>
-            <h4 class="roboto-serif four">{{ store.springAvg }}&deg; F</h4>
-        </div>
-        <div class="callout">
+            <p class="roboto-serif four">{{ store.springAvg }}&deg; F</p>
+        </hgroup>
+        <hgroup class="callout">
             <h3 class="roboto-serif six">Summer</h3>
-            <h4 class="roboto-serif four">{{ store.summerAvg }}&deg; F</h4>
-        </div>
-        <div class="callout">
+            <p class="roboto-serif four">{{ store.summerAvg }}&deg; F</p>
+        </hgroup>
+        <hgroup class="callout">
             <h3 class="roboto-serif six">Autumn</h3>
-            <h4 class="roboto-serif four">{{ store.fallAvg }}&deg; F</h4>
-        </div>
-        <div class="callout">
+            <p class="roboto-serif four">{{ store.fallAvg }}&deg; F</p>
+        </hgroup>
+        <hgroup class="callout">
             <h3 class="roboto-serif six">Winter</h3>
-            <h4 class="roboto-serif four">{{ store.winterAvg }}&deg; F</h4>
-        </div>
+            <p class="roboto-serif four">{{ store.winterAvg }}&deg; F</p>
+        </hgroup>
     </section>
 </template>
 
