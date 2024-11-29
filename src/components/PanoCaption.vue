57f4b445446645b35b661c7cf99f96a65d61486f
Sean Kelliher <sbkelliher@icloud.com>
2024-11-29 12:14:41 -0500

    Text changes. Shorter caption.

--- src/components/PanoCaption.vue
+++ src/components/PanoCaption.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="pano-caption roboto-serif four">Petty Officer 3rd Class Jonathan Szylobry, a US Marine, on the summit of Mt. Washington in North Conway, New Hampshire. At 6,288 feet (1,916.6 meters) above sea level, it is the highest point in the Northeast United States. Photo: <a href="https://commons.wikimedia.org/wiki/File:USMC-080103-M-0000M-01.jpg" target="_blank">Department of Defense</a>.</div>
+    <div class="pano-caption roboto-serif four">US Marine Jonathan Szylobry summits Mt. Washington in North Conway, NH. At 6,288 ft, it's the highest point in the Northeast US. Photo: <a href="https://commons.wikimedia.org/wiki/File:USMC-080103-M-0000M-01.jpg" target="_blank">Department of Defense</a></div>
 </template>
 
 <style scoped>
