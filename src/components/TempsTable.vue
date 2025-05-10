c4833a57416d68c0c83e98356bc35c3d761d848c
Sean Kelliher <sbkelliher@icloud.com>
2025-05-09 20:02:43 -0400

    Make changes to table to avoid Vite warning.

--- src/components/TempsTable.vue
+++ src/components/TempsTable.vue
@@ -10,45 +10,53 @@ import { store } from "../composables/store.js";
         </div>
 
         <table>
-            <th>
-                <td class="roboto-serif six">Year</td>
-                <td class="roboto-serif six">Jan</td>
-                <td class="roboto-serif six">Feb</td>
-                <td class="roboto-serif six">Mar</td>
-                <td class="roboto-serif six">Apr</td>
-                <td class="roboto-serif six">May</td>
-                <td class="roboto-serif six">Jun</td>
-            </th>
-            <tr v-for="(year, index) in store.years" :key="year">
-                <td class="roboto-serif four">{{ year.substring(1,5) }}</td>
-                <td class="roboto-serif four">{{ store.jans[index] }}</td>
-                <td class="roboto-serif four">{{ store.febs[index] }}</td>
-                <td class="roboto-serif four">{{ store.mars[index] }}</td>
-                <td class="roboto-serif four">{{ store.aprs[index] }}</td>
-                <td class="roboto-serif four">{{ store.mays[index] }}</td>
-                <td class="roboto-serif four">{{ store.juns[index] }}</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th class="roboto-serif six">Year</th>
+                    <th class="roboto-serif six">Jan</th>
+                    <th class="roboto-serif six">Feb</th>
+                    <th class="roboto-serif six">Mar</th>
+                    <th class="roboto-serif six">Apr</th>
+                    <th class="roboto-serif six">May</th>
+                    <th class="roboto-serif six">Jun</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(year, index) in store.years" :key="year">
+                    <td class="roboto-serif four">{{ year.substring(1,5) }}</td>
+                    <td class="roboto-serif four">{{ store.jans[index] }}</td>
+                    <td class="roboto-serif four">{{ store.febs[index] }}</td>
+                    <td class="roboto-serif four">{{ store.mars[index] }}</td>
+                    <td class="roboto-serif four">{{ store.aprs[index] }}</td>
+                    <td class="roboto-serif four">{{ store.mays[index] }}</td>
+                    <td class="roboto-serif four">{{ store.juns[index] }}</td>
+                </tr>
+            </tbody>
         </table>
 
         <table>
-            <th>
-                <td class="roboto-serif six">Year</td>
-                <td class="roboto-serif six">Jul</td>
-                <td class="roboto-serif six">Aug</td>
-                <td class="roboto-serif six">Sep</td>
-                <td class="roboto-serif six">Oct</td>
-                <td class="roboto-serif six">Nov</td>
-                <td class="roboto-serif six">Dec</td>
-            </th>
-            <tr v-for="(year, index) in store.years" :key="year">
-                <td class="roboto-serif four">{{ year.substring(1,5) }}</td>
-                <td class="roboto-serif four">{{ store.juls[index] }}</td>
-                <td class="roboto-serif four">{{ store.augs[index] }}</td>
-                <td class="roboto-serif four">{{ store.seps[index] }}</td>
-                <td class="roboto-serif four">{{ store.octs[index] }}</td>
-                <td class="roboto-serif four">{{ store.novs[index] }}</td>
-                <td class="roboto-serif four">{{ store.decs[index] }}</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th class="roboto-serif six">Year</th>
+                    <th class="roboto-serif six">Jul</th>
+                    <th class="roboto-serif six">Aug</th>
+                    <th class="roboto-serif six">Sep</th>
+                    <th class="roboto-serif six">Oct</th>
+                    <th class="roboto-serif six">Nov</th>
+                    <th class="roboto-serif six">Dec</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(year, index) in store.years" :key="year">
+                    <td class="roboto-serif four">{{ year.substring(1,5) }}</td>
+                    <td class="roboto-serif four">{{ store.juls[index] }}</td>
+                    <td class="roboto-serif four">{{ store.augs[index] }}</td>
+                    <td class="roboto-serif four">{{ store.seps[index] }}</td>
+                    <td class="roboto-serif four">{{ store.octs[index] }}</td>
+                    <td class="roboto-serif four">{{ store.novs[index] }}</td>
+                    <td class="roboto-serif four">{{ store.decs[index] }}</td>
+                </tr>
+            </tbody>
         </table>
         <div class="chart-table-caption roboto-serif four">Data from the Mt. Washington Observatory, a weather station located on the mountain's summit. The Observatory monitors weather year round.</div>
     </div>
